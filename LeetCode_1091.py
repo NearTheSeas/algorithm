9764d0c6af93debf1249be316bc0da2cba278604
ColorCC <xuwenlong.xinguan@gmail.com>
2021-07-09 22:24:44 +0800

    高级搜索

--- LeetCode_1091.py
+++ LeetCode_1091.py
@@ -1,4 +1,24 @@
 """ 
 https://leetcode-cn.com/problems/shortest-path-in-binary-matrix/
 1091. 二进制矩阵中的最短路径
+最短路径 → BFS
+还可以双向
 """
+from typing import List
+
+
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        if grid[0][0] or grid[-1][-1]:
+            return -1
+        q = [(0, 0, 1)]
+        grid[0][0] = 1
+        for i, j, d in q:
+            if i == n-1 and j == n-1:
+                return d
+            for x, y in [(i-1, j-1), (i-1, j), (i-1, j+1), (i, j-1), (i, j+1), (i+1, j-1), (i+1, j), (i+1, j+1)]:
+                if 0 <= x < n and 0 <= y < n and not grid[x][y]:
+                    grid[x][y] = 1
+                    q.append((x, y, d+1))
+        return -1
--- LeetCode_773.py
+++ LeetCode_773.py
@@ -1,4 +1,42 @@
 """ 
 https://leetcode-cn.com/problems/sliding-puzzle/
 773. 滑动谜题
+记录二位数组中可移动的下标位置
+[0,1,2]
+[3,4,5]
+找到当前“0”所在位置，遍历其邻居
+广度优先遍历
 """
+from typing import List
+import collections
+
+
+class Solution:
+    def slidingPuzzle(self, board: List[List[int]]) -> int:
+        neighbours = {
+            0: [1, 3],
+            1: [0, 2, 4],
+            2: [1, 5],
+            3: [0, 4],
+            4: [1, 3, 5],
+            5: [2, 4]
+        }
+
+        state = "".join(str(c) for c in board[0]+board[1])
+        start = state.index('0')
+        visited = set()
+        q = collections.deque([(start, state, 0)])
+        while q:
+            cur, state, steps = q.popleft()
+            if state == '123450':
+                return steps
+            elif state in visited:
+                continue
+            else:
+                visited.add(state)
+                for next in neighbours[cur]:
+                    tmp = list(state)
+                    tmp[cur], tmp[next] = tmp[next], tmp[cur]
+                    tmp = "".join(tmp)
+                    q.append((next, tmp, steps+1))
+        return -1
