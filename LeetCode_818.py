6524cc3f6165608bbf9ef52c13897614845e8bed
ColorCC <xuwenlong.xinguan@gmail.com>
2021-07-13 13:03:45 +0800

    Update LeetCode_818.py

--- LeetCode_818.py
+++ LeetCode_818.py
@@ -10,20 +10,19 @@ k-1个A到达S(k-1)，掉头R，回退一定步数A，再前进R， 前进一定
 
 class Solution:
     def racecar(self, target: int) -> int:
-        dp = [1, 4] + [0]*(target)
-        k, S = 2, 3  # S是位置，k是连续加速次数，S记录连续k个A指令达到的位置
-        for i in range(3, target):
-            if i == S:
-                dp[i] = k
-                k += 1
-                S = (1 << k) - 1  # 2^k - 1
-            else:
-                #  情况1：连续k个A后，回退
-                dp[i] = k+1+dp[S-i]
-                # 情况2：连续k-1个A后，回退(0/1/.../k-2)步后，再前进
-                for back in range(k-1):
-                    # 回退后还需前进的距离：i+S(back)-S(k-1)
-                    d = i + (1 << back) - (1 << (k-1))
-                    dp[i] = min(dp[i], (k-1)+2+back+dp[d])
+        dp = [0, 1, 4] + [float('inf')]*(target)
+        for t in range(3, target+1):
+            k = t.bit_length()
+            # k个A 刚好到达t
+            if t == 2**k-1:
+                dp[t] = k
+                continue
+            # 倒退j步 2个R
+            for j in range(k-1):
+                # 前进k-1步，回退j步，再前进
+                dp[t] = min(dp[t], dp[t-(2**(k-1)-2**j)] + k-1 + j + 2)
+            if 2**k - 1 - t < t:
+                # 前进k步 超过了t
+                dp[t] = min(dp[t], dp[2**k-1-t]+k+1)
 
         return dp[target]
